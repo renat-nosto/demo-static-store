4947737471b352be15124e501933030b77348720
Renat Vasilyev <renat.vasilyev@nosto.com>
2022-06-29 14:34:56 +0300

    push 2

--- include.js
+++ include.js
@@ -1,5 +1,5 @@
 if(location.origin === "https://renat-nosto.github.io") {
-  document.write(`<scr`+`ipt src="https://connect.staging.nosto.com/include/qvuhs4m3"></scr`+`ipt>`)
+  document.write(`<scr`+`ipt src="https://connect.staging.nosto.com/include/qvuhs4m3?"></scr`+`ipt>`)
 } else {
   document.write(`<scr`+`ipt src="https://my.dev.nos.to/include/renat"></scr`+`ipt>`)
 }
