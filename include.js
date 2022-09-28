ac68b84a08669649fc87cc9f96023dae7b37af2d
Renat Vasilyev <renat.vasilyev@nosto.com>
2022-09-28 15:07:44 +0300

    nice improvements + sku

--- include.js
+++ include.js
@@ -1,5 +1,22 @@
+document.head.insertAdjacentHTML('beforeend', `<style>
+    .nosto_product span {
+      display: block;
+        margin-left: 1em;
+    }
+    .nosto_product span:before {
+        content: attr(class)' : ';
+    }
+  </style>
+`)
+
 if(location.origin === "https://renat-nosto.github.io") {
   document.write(`<scr`+`ipt src="https://connect.staging.nosto.com/include/qvuhs4m3?"></scr`+`ipt>`)
 } else {
   document.write(`<scr`+`ipt src="https://my.dev.nos.to/include/renat"></scr`+`ipt>`)
 }
+
+setTimeout(() => {
+  Array.from(document.querySelectorAll(".image_url")).forEach(e => {
+    e.insertAdjacentHTML('afterend', `<img src="${e.textContent}" style="max-width: 300px; max-height: 300px">`)
+  })
+}, 10)
